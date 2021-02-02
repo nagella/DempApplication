4f0486a6585277d720cb546dfe53860c4d7db8d9
Maheswar Nagella <maheswar.nagella@gmail.com>
2021-02-02 01:03:48 -0500

    SpringBoot Commits

---
+++ src/main/java/com/hyd/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.hyd.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("***** Application Started *****");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
